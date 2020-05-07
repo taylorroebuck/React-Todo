adeff62b6efaf192c7d5024461b6a6b9fd9f6f47
Taylor Roebuck <taylornroebuck@gmail.com>
2020-05-06 19:12:48 -0700

    wrote constructor function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,24 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: "Bowl with Walter",
+          id: 1,
+          completed: false
+        },
+        {
+          task: "Figure out where Bunny Lebowski is",
+          id: 2,
+          completed: false
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
